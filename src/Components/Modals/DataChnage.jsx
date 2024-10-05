af7d19b731d35fc3da6c507623479977c9d33675
Retsos <GeorgeRets@gmail.com>
2024-10-05 14:07:41 +0300

    Neo Modal gia allagi AllagiStoixeiwn

---
+++ src/Components/Modals/DataChnage.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import './DataChange.css';
+
+
+
+export default function DataChange({ showModal2, setShowModal2 }) {
+
+
+
+    const handleClose = () => setShowModal2(false);
+
+
+
+    useEffect(() => {
+        if (showModal2) {
+            document.body.classList.add('modal-open');
+            const backdrop = document.createElement('div');
+            backdrop.className = 'modal-backdrop fade show';
+            document.body.appendChild(backdrop);
+        } else {
+            document.body.classList.remove('modal-open');
+            const backdrop = document.querySelector('.modal-backdrop');
+            if (backdrop) {
+                backdrop.remove();
+            }
+        }
+        return () => {
+            document.body.classList.remove('modal-open');
+            const backdrop = document.querySelector('.modal-backdrop');
+            if (backdrop) {
+                backdrop.remove();
+            }
+        };
+    }, [showModal2]);
+
+
+
+    if (!showModal2) return null;
+    return (
+        <>
+            <div className="modal show" style={{ display: 'block'}} tabIndex="-1" aria-labelledby="exampleModalLabel">
+                <div className="modal-dialog-centered modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title fs-5" id="exampleModalLabel">Modal title</h3>
+                            <button type="button" onClick={handleClose} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn custome-button" onClick={handleClose}>Αποθήκευση Αλλαγών</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
